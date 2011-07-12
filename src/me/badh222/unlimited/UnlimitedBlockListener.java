af209afe1832e22fae92dc2793ca729c9cc98092
Ashour225 <runscapepkerz@gmail.com>
2011-07-12 13:42:01 -0700

    Edited src/me/badh222/unlimited/UnlimitedBlockListener.java via GitHub

--- src/me/badh222/unlimited/UnlimitedBlockListener.java
+++ src/me/badh222/unlimited/UnlimitedBlockListener.java
@@ -1,5 +1,6 @@
 package me.badbh222.Unlimited;
 
+import java.util.HashMap;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -7,57 +8,58 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockListener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
-public class UnlimitedBlockListener extends BlockListener{
-	
-	private final Unlimited plugin;
-	public UnlimitedBlockListener(final Unlimited plugin){
-		
-		this.plugin = plugin;
-	}
+public class UnlimitedBlockListener extends BlockListener
+{
+  private final Unlimited plugin;
 
-	@SuppressWarnings("deprecation")
-	public void onBlockPlace(BlockPlaceEvent event){
-		
-		if (event.isCancelled()){
-			return;
-		}
-		
-		Block block = event.getBlock();
-		Player player = (Player)event.getPlayer();
-		int ci = block.getTypeId();
-		if (plugin.items.containsKey(ci)){
-			ci = plugin.items.get(ci);
-		}
-		
-		if (plugin.UB.containsKey(player.getName())){
-			plugin.UBints = plugin.UB.get(player.getName());
-			if (plugin.UBints.containsKey(ci)){
-				player.getInventory().addItem(new ItemStack(ci, 1));
-				player.updateInventory();
-			}
-		}
-	}
-	
-	public void onBlockBreak(BlockBreakEvent event){
-		
-		if (event.isCancelled()){
-			return;
-		}
-		
-		Block block = event.getBlock();
-		Player player = (Player)event.getPlayer();
-		int ci = block.getTypeId();
-		if (plugin.items.containsKey(ci)){
-			ci = plugin.items.get(ci);
-		}
-		
-		if (plugin.UB.containsKey(player.getName())){
-			plugin.UBints = plugin.UB.get(player.getName());
-			if (plugin.UBints.containsKey(ci)){
-				block.setType(Material.AIR);
-				event.setCancelled(true);
-			}
-		}
-	}
+  public UnlimitedBlockListener(Unlimited plugin)
+  {
+    this.plugin = plugin;
+  }
+
+  public void onBlockPlace(BlockPlaceEvent event)
+  {
+    if (event.isCancelled()) {
+      return;
+    }
+
+    Block block = event.getBlock();
+    Player player = event.getPlayer();
+    int ci = block.getTypeId();
+    if (this.plugin.items.containsKey(Integer.valueOf(ci))) {
+      ci = ((Integer)this.plugin.items.get(Integer.valueOf(ci))).intValue();
+    }
+
+    if (this.plugin.UB.containsKey(player.getName())) {
+      this.plugin.UBints = ((HashMap)this.plugin.UB.get(player.getName()));
+      if (this.plugin.UBints.containsKey(Integer.valueOf(ci))) {
+        player.getInventory().addItem(new ItemStack[] { new ItemStack(ci, 1) });
+        player.updateInventory();
+      }
+    }
+  }
+
+  public void onBlockBreak(BlockBreakEvent event)
+  {
+    if (event.isCancelled()) {
+      return;
+    }
+
+    Block block = event.getBlock();
+    Player player = event.getPlayer();
+    int ci = block.getTypeId();
+    if (this.plugin.items.containsKey(Integer.valueOf(ci))) {
+      ci = ((Integer)this.plugin.items.get(Integer.valueOf(ci))).intValue();
+    }
+
+    if (this.plugin.UB.containsKey(player.getName())) {
+      this.plugin.UBints = ((HashMap)this.plugin.UB.get(player.getName()));
+      if (this.plugin.UBints.containsKey(Integer.valueOf(ci))) {
+        block.setType(Material.AIR);
+        event.setCancelled(true);
+      }
+    }
+  }
 }
